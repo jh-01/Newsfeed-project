a466f7e47f7962411783436c7b1418dce548f629
jh-01 <were1117@sm.ac.kr>
2025-05-27 10:20:17 +0900

    Init: 프로젝트 생성

---
+++ src/test/java/org/example/newsfeedproject/NewsfeedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.newsfeedproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
