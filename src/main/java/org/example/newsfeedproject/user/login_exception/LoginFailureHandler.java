dc01b84f2cf39b4fbb27d9619c1a436ca8544ba2
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-30 17:47:05 +0900

    feat : 로그인 - PasswordMismatch 예외핸들러 처리

---
+++ src/main/java/org/example/newsfeedproject/user/login_exception/LoginFailureHandler.java
@@ -0,0 +1,17 @@
+package org.example.newsfeedproject.user.login_exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class LoginFailureHandler {
+
+    // 비밀번호 틀리면 401 반환
+    @ExceptionHandler(PasswordMismatchException.class)
+    public ResponseEntity<String> PasswordMismatchException (PasswordMismatchException e) {
+        return new ResponseEntity<>("로그인 실패 : 비밀번호가 틀립니다.",HttpStatus.UNAUTHORIZED);
+    }
+
+}
