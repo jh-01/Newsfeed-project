9f888eb06bf9054f2c57321d88cd86c394945355
jh-01 <were1117@sm.ac.kr>
2025-06-01 16:34:45 +0900

    bug: 오류 수정
    - 댓글 수정 오류 해결
    - 댓글 수정, 삭제 매핑 수정
    - 예외가 적절한 상태코드를 반환하도록 수정

--- src/main/java/org/example/newsfeedproject/comment/exception/UserDeactivatedException.java
+++ src/main/java/org/example/newsfeedproject/comment/exception/UserDeactivatedException.java
@@ -1,5 +1,9 @@
 package org.example.newsfeedproject.comment.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class UserDeactivatedException extends RuntimeException{
     public UserDeactivatedException(Long id){
         super("탈퇴한 유저입니다. id = " + id);
