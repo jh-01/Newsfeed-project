6895241115caaa1ea4a778a8591c208d4f4bbf96
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-06-02 16:27:53 +0900

    fix - 예외핸들러 메서드명 변경

--- src/main/java/org/example/newsfeedproject/user/login_exception/LoginFailureHandler.java
+++ src/main/java/org/example/newsfeedproject/user/login_exception/LoginFailureHandler.java
@@ -10,13 +10,13 @@ public class LoginFailureHandler {
 
     // 비밀번호 틀리면 401 반환
     @ExceptionHandler(PasswordMismatchException.class)
-    public ResponseEntity<String> PasswordMismatchException (PasswordMismatchException e) {
+    public ResponseEntity<String> passwordMismatchException (PasswordMismatchException e) {
         return new ResponseEntity<>("로그인 실패 : 비밀번호가 틀립니다.",HttpStatus.UNAUTHORIZED);
     }
 
     // 삭제된 유저면 400 반환
     @ExceptionHandler(DeletedUserException.class)
-    public ResponseEntity<String> DeletedUserException (DeletedUserException e) {
+    public ResponseEntity<String> deletedUserException (DeletedUserException e) {
         return new ResponseEntity<>("로그인 실패 : 탈퇴한 유저입니다.", HttpStatus.BAD_REQUEST);
     }
 
